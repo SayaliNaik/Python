064521348c465722cd5367af84efabfa524c3162
Sayali Naik <sayalianaik21@gmail.com>
2018-04-01 20:40:10 -0700

    Basic If Else loop

---
+++ If_Else.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    n = int(raw_input())
+
+
+if(n%2==0):
+    if (2<=n<=5):
+        print("Not Weird")
+    if (5<n<=20):
+        print("Weird")
+    if (20<n):
+        print("Not Weird")
+else:
+    print("Weird")
+    
