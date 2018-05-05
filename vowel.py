428be7104e869b49c13f3eccf4c3d035a3ef7a89
Sayali Naik <sayalianaik21@gmail.com>
2018-05-05 12:26:22 -0700

    Delete vowel.py

--- vowel.py
+++
@@ -1,8 +0,0 @@
-c = input("Enter any alphabet\n")
-if ((c=='a')|(c=='e')|(c=='i')|(c=='o')|(c=='u')):
-    print("It's a vowel")
-else:
-    print("It's a consonant")
-
-print("Press enter to exit")       
-    
