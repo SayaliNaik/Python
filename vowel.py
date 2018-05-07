50cd8048be91d4498a19ed124464446cd06f528a
SayaliNaik <sayalianaik21@gmail.com>
2018-05-07 15:00:31 -0700

    Find the vowel

---
+++ vowel.py
@@ -0,0 +1,8 @@
+c = input("Enter any alphabet\n")
+if ((c=='a')|(c=='e')|(c=='i')|(c=='o')|(c=='u')):
+    print("It's a vowel")
+else:
+    print("It's a consonant")
+
+print("Press enter to exit")       
+    
